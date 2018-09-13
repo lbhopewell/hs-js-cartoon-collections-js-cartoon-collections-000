7b51d462ee47b18cc57c4ec0d6fc471146a768a7
Bruce Hopewell <lbhopewell@outlook.com>
2018-09-13 19:56:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 function dwarfRollCall(dwarves) {
+  var dwarvesString = "";
+  for(let i=0; i < dwarves.length; i++){
+    dwarvesString += i+1+". "+dwarves[i]+" ";
+  }
+  return dwarvesString
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var planeteer = [];
+  for(let i=0; i < planeteerCalls.length; i++){
+    planeteer.push(planeteerCalls[i].toUpperCase() + "!")
+  }
+  return planeteer
 }
 
 function longPlaneteerCalls(words) {
+  for(let i=0; i < words.length; i++){
+    if(words[i].length > 4){
+      return true
+    } else
+    return false
+  }
 }
 
 function findTheCheese (foods) {
+  for (let i=0; i < foods.length; i++){
+    if(foods[i] === "cheddar") return "cheddar";
+    if(foods[i] === "gouda") return "gouda";
+    if(foods[i] === "camembert") return "camembert";
+  }
+  return "no cheese!"
 }
